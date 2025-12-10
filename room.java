abee5b137612729c9e55706b0e0ee2c119b198d0
Su <suelagozz@gmail.com>
2025-12-10 19:50:11 +0300

    Add getters and setters for room properties

--- room.java
+++ room.java
@@ -21,4 +21,36 @@ public class Room {
     public String getRoomNumber() {
         return roomNumber;
     }
+      public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    public String getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+   public void printAvailable() {
+       if (isAvailable) {
+           System.out.println("Room is available");
+       } else {
+           System.out.println("Room is not available");
+       }
+   }
+
+
+    }
+
 }
