e5bcbcdc90559e325c3f41f4798ed04712e5b557
Su <suelagozz@gmail.com>
2025-12-22 18:53:10 +0300

    added available room retrieval
    Added methods for creating reservations and retrieving available rooms.

--- Booking.java
+++ Booking.java
@@ -1,45 +1,75 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Booking {
 
-    // Collections - Tüm verileri sakla
+
     private ArrayList<Room> rooms;
     private ArrayList<Customer> customers;
     private ArrayList<Reservation> reservations;
 
-    // Constructor
+
     public Booking() {
         rooms = new ArrayList<>();
         customers = new ArrayList<>();
         reservations = new ArrayList<>();
-        initializeRooms();  // Başlangıç odaları ekle
+        initializeRooms();
     }
 
 
     private void initializeRooms() {
-        addRoom(new StandardRoom("101", "2", 100.0, true));
-        addRoom(new StandardRoom("102", "2", 100.0, true));
-        addRoom(new StandardRoom("103", "4", 120.0, true));
+        addRoom(new StandardRoom("101", 2, 100.0, true));
+        addRoom(new StandardRoom("102", 2, 100.0, true));
+        addRoom(new StandardRoom("103", 4, 120.0, true));
 
-        // SuiteRoom(roomNumber, capacity, price, isAvailable, luxuryFee, suiteLevel, hasLivingRoom)
-        addRoom(new SuiteRoom("201", "4", 250.0, true, 50, "Deluxe", true));
-        addRoom(new SuiteRoom("202", "6", 300.0, true, 75, "Presidential", true));
+        addRoom(new SuiteRoom("201", 2, 250.0, true, 50, "Deluxe", true));
+        addRoom(new SuiteRoom("202", 6, 300.0, true, 75, "Presidential", true));
     }
-    private void addRoom(Room room) {
+
+    public void addRoom(Room room) {
         rooms.add(room);
     }
+    void addCustomer(Customer customer){
+        customers.add(customer);
+    }
+    public void createReservation(String reservationId, Customer customer, Room room) {
+
+        if (!room.isAvailable()) {
+            System.out.println("Room is not available.");
+            return;
+        }
+
+        LocalDate checkIn = LocalDate.now();
+        LocalDate checkOut = checkIn.plusDays(3); // random number for testing
+
+        Reservation reservation =
+                new Reservation(reservationId, customer, room, checkIn, checkOut);
+
+        reservations.add(reservation);
+    }
+
+
     public ArrayList<Room> getRooms() {
         return rooms;
     }
+
     public ArrayList<Customer> getCustomers() {
         return customers;
     }
+
     public ArrayList<Reservation> getReservations() {
         return reservations;
     }
-    private void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
-    
-}
 
+    public ArrayList<Room> getAvailableRooms() {
+        ArrayList<Room> availableRooms = new ArrayList<>();
+
+        for (Room room : rooms) {
+            if (room.isAvailable()) {
+                availableRooms.add(room);
+            }
+        }
+        return availableRooms;
+    }
+
+}
